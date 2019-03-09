34da11d61edb840f023687192a04be0a6caedbdd
李鸿达 <963140097@qq.com>
2019-03-09 16:18:11 +0800

    提交测试代码

--- app/src/main/java/com/study/netmoudle/MainActivity.java
+++ app/src/main/java/com/study/netmoudle/MainActivity.java
@@ -1,11 +1,13 @@
 package com.study.netmoudle;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.study.netmoudle.bean.TestBean;
 import com.study.netmoudle.usecase.TestUsecase;
 
 import io.reactivex.disposables.Disposable;
@@ -24,21 +26,29 @@ public class MainActivity extends AppCompatActivity {
         tvResult = findViewById(R.id.tv_result);
         btnTest = findViewById(R.id.btn_test);
         testUsecase = new TestUsecase();
-        btnTest.setOnClickListener(view -> disposable = testUsecase.getTest().subscribe(testBean -> {
-//            if (testBean!=null&&testBean.getData()!=null){
-//                TestBean.DataBean data= testBean.getData();
-//                String name = data.getName();
-//                String sex = data.getSex();
-//                int age = data.getAge();
-//                tvResult.setText(name+"  "+sex+"  "+age);
-//            }
-        }, new Consumer<Throwable>() {
-           @Override
-           public void accept(Throwable throwable) throws Exception {
-               Log.i("LHD","请求失败 = "+throwable.getMessage());
-               throwable.printStackTrace();
-           }
-       }));
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 disposable = testUsecase.getTest().subscribe(new Consumer<TestBean>() {
+                    @Override
+                    public void accept(TestBean testBean) throws Exception {
+                        if (testBean!=null&&testBean.getData()!=null){
+                            TestBean.DataBean data= testBean.getData();
+//                            String name = data.getName();
+                            String sex = data.getSex();
+                            int age = data.getAge();
+//                            tvResult.setText(name+"  "+sex+"  "+age);
+                        }
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.i("LHD","请求失败 = "+throwable.getMessage());
+                        throwable.printStackTrace();
+                    }
+                });
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/study/netmoudle/bean/TestBean.java
+++ app/src/main/java/com/study/netmoudle/bean/TestBean.java
@@ -6,5 +6,63 @@ package com.study.netmoudle.bean;
 public class TestBean {
 
 
+    /**
+     * message : success
+     * data : {"sex":"woman","age":18}
+     * code : 0
+     */
 
+    private String message;
+    private DataBean data;
+    private int code;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public static class DataBean {
+        /**
+         * sex : woman
+         * age : 18
+         */
+
+        private String sex;
+        private int age;
+
+        public String getSex() {
+            return sex;
+        }
+
+        public void setSex(String sex) {
+            this.sex = sex;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
 }
